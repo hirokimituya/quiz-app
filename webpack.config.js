ea524271dd69b2b6af5f5698096b9fd89aee641e
hirokimituya <hiromitu819@yahoo.co.jp>
2021-07-21 20:54:34 +0900

    jsファイルをgzip化

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+    plugins: [
+        new CompressionPlugin({
+          filename: '[path][base].gz[query]',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.html$|\.svg$/,
+          threshold: 10240,
+          minRatio: 0.8,
+        })
+    ],
     resolve: {
         alias: {
             '@': path.resolve('resources/js'),
